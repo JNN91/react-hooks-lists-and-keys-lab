f18562154596e0ceb0cb49ccab49a7723cd8afc8
Johnson <johnson.nganga@student.moringaschool.com>
2022-10-12 17:56:44 +0300

    Bitcoin

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const timeProject = projects.map(project =>{
+    return <React.Fragment key={project.id}>{ProjectItem(project)}</React.Fragment>
+  }) 
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{timeProject}</div>
     </div>
   );
 }
